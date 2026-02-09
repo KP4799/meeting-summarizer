8e10be39dc062dcd3281967aa0b9ac0074664c97
Krutarth Patel <116948722+KP4799@users.noreply.github.com>
2026-02-09 11:34:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from speech import listen
 from translator import translate_text,translate_summary
 from summarization import summarize
 
-print("Inside main")
-
 #while True:
 input = listen()
 translate_text(input)
